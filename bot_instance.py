60df130ca929cafb2262f5d616c95eae3cc64c34
damnurmum <its.bruharas@gmail.com>
2026-07-29 14:03:16 +0300

    feat: add cloudflare proxy support

--- bot_instance.py
+++ bot_instance.py
@@ -1,6 +1,12 @@
 # load telebot libs
 from telebot import TeleBot
 from telebot import ExceptionHandler as BaseExceptionHandler
+from telebot import apihelper
+
+# set up warp connect proxy
+apihelper.proxy = {
+    'https': 'socks5h://127.0.0.1:40000'
+}
 
 # load config
 from config import BOT_TOKEN, ADMIN
