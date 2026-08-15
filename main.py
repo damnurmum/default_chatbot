eb88df1de6ab96c7dc01b9756d7c26df7dba612b
damnurmum <its.bruharas@gmail.com>
2026-08-15 03:42:56 +0300

    perm: add edited_message, callback_query permission to ALLOWED_UPDATES

--- main.py
+++ main.py
@@ -15,6 +15,8 @@ MEDIA_RESOURCES = {
 # Telegram update types the bot actually listens to
 ALLOWED_UPDATES = [
     "message",
+    "edited_message",
+    "callback_query",
     "channel_post",
     "edited_channel_post",
     "message_reaction",
@@ -73,8 +75,7 @@ if __name__ == "__main__":
     if config.AUDIT_FOR_LOG_CHAT: import audit_handlers  # noqa: F401
     import handlers  # noqa: F401
 
-    # MTProto watcher runs beside TeleBot and catches deleted channel posts
-
+    # mtproto watcher runs beside telebot and catches deleted channel posts
     if config.AUDIT_FOR_LOG_CHAT and config.DELETION_AUDIT_MTPROTO: from mtproto_audit import start_deletion_audit; start_deletion_audit()
 
     # listen only to updates we actually use; less noise, more speed
